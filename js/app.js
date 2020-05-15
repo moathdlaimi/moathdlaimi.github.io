76211a170c4873ab3a79db83c1268a836fdf8eff
Moath <moath.dlaii@gmail.com>
2020-05-15 16:03:34 -0700

    App functioning needs design

--- js/app.js
+++ js/app.js
@@ -5,25 +5,23 @@ $(() => {
 
   $('.btn').on('click', (event) =>{
     event.preventDefault();
-    let clickedBtn = $(event.targent).attr('id');
-    if( clickedBtn === 'location'){
 
-    const findLocation = () => {
+    if($(event.currentTarget).attr('id') === 'location' ){
     if ("geolocation" in navigator){
     	navigator.geolocation.getCurrentPosition(function(position){
         let lon = position.coords.longitude;
         let lat = position.coords.latitude;
 
           $.ajax({
-            url:`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&q=${city}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
+            url:`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
 
         }).then((data) => {
 
-           let city = data.name
+           let cit = data.name
            const kelvin = data.main.temp;
            const celsius = kelvin - 273;
            let fahrenheit = celsius * (9/5) + 32;
-           console.log(`The temperature in ${city} is ${parseInt(fahrenheit)}`);
+           const $p = $('<p>').html(`The temperature in ${cit} is ${parseInt(fahrenheit)}`).addClass('paragraph').appendTo('.reports');
 
 
         },
@@ -34,10 +32,10 @@ $(() => {
     }else{
     	alret("Browser doesn't support geolocation!");
     }
-    }
+
   }
   else {
-    let city = $('input[type="text"]').val();
+    let city = $('input[type="text"]').val()
     $.ajax({
       url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
 
@@ -47,7 +45,8 @@ $(() => {
      const kelvin = data.main.temp;
      const celsius = kelvin - 273;
      let fahrenheit = celsius * (9/5) + 32;
-     console.log(`The temperature in ${cit} is ${parseInt(fahrenheit)}`);
+     const $p = $('<p>').html(`The temperature in ${cit} is ${parseInt(fahrenheit)}`).addClass('paragraph').appendTo('.reports');
+
 
 
   },
