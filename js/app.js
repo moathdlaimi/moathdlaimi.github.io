d371749258bde4b6b189bbdbcb3ca837ab378fc5
Moath <moath.dlaii@gmail.com>
2020-05-15 15:17:21 -0700

    geolocation button

--- js/app.js
+++ js/app.js
@@ -1,22 +1,47 @@
+
+// *************
+// *************
 $(() => {
 
-  $('.search').on('click', (event) =>{
+  $('.btn').on('click', (event) =>{
     event.preventDefault();
+    const findLocation = () => {
+    if ("geolocation" in navigator){
+    	navigator.geolocation.getCurrentPosition(function(position){
+        let lon = position.coords.longitude;
+        let lat = position.coords.latitude;
 
-  })// onlick closing
+          $.ajax({
+            url:`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&q=${city}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
 
-  let city = 'ISTANBUL';
+        }).then((data) => {
 
-//   $.ajax({
-//     //url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
-//
-// }).then((data) => {
-// console.log(data);
-// },
-// ()=>{
-//   console.log('error');
-// })
+           let city = data.name
+           const kelvin = data.main.temp;
+           const celsius = kelvin - 273;
+           let fahrenheit = celsius * (9/5) + 32;
+           console.log(`The temperature in ${city} is ${parseInt(fahrenheit)}`);
 
+
+        },
+        ()=>{
+          console.log('error');
+        })
+    		});
+    }else{
+    	alret("Browser doesn't support geolocation!");
+    }
+    }
+
+    let city = $('input[type="text"]').val();
+    let currentLocation = $(event.target).on('click', findLocation)
+
+
+
+
+
+
+})// event closing
 
 
 
