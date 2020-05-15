145ee3288708374838c90079ce62e49524702417
Moath <moath.dlaii@gmail.com>
2020-05-15 13:02:27 -0700

    -first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(() => {
+
+  $('.search').on('click', (event) =>{
+    event.preventDefault();
+
+  })// onlick closing
+
+  let city = 'ISTANBUL';
+
+//   $.ajax({
+//     //url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=80dd0dc92d5ff0e086d4ae0eeb8918a9`,
+//
+// }).then((data) => {
+// console.log(data);
+// },
+// ()=>{
+//   console.log('error');
+// })
+
+
+
+
+}) //onload closing
